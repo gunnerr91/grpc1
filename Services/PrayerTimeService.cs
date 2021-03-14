3d31bc57910a57982fe696ec8c5faac8c305f331
gunnerr91 <blissvortex@hotmail.com>
2021-03-14 17:29:18 +0000

    first commit

---
+++ Services/PrayerTimeService.cs
@@ -0,0 +1,35 @@
+﻿using Grpc.Core;
+using PrayerApiContract;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GRPC1
+{
+    public class PrayerTimeService : prayertime.prayertimeBase
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public PrayerTimeService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public override async Task<ResponseForPrayerTimesInCity> GetPrayerTimesByCity(RequestForPrayerTimesInCity request, ServerCallContext context)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var response = await client.GetStringAsync($"https://api.pray.zone/v2/times/today.json?city={request.City}&school={request.School}");
+
+            var prayerTimes = PrayerTimes.FromJson(response);
+
+            return new ResponseForPrayerTimesInCity
+            {
+                Asr = prayerTimes.Results.Datetime[0].Times.Asr,
+                Dhuhr = prayerTimes.Results.Datetime[0].Times.Dhuhr,
+                Fajr = prayerTimes.Results.Datetime[0].Times.Fajr,
+                Isha = prayerTimes.Results.Datetime[0].Times.Isha,
+                Maghrib = prayerTimes.Results.Datetime[0].Times.Maghrib
+            };
+        }
+    }
+}
