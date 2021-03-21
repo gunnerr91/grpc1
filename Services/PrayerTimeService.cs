d33fd11827ecd5a4c23e980233d53a902b651d4e
gunnerr91 <blissvortex@hotmail.com>
2021-03-21 07:52:02 +0000

    updated contract to include multi stream client and server requests/responses

--- Services/PrayerTimeService.cs
+++ Services/PrayerTimeService.cs
@@ -37,6 +37,24 @@ namespace GRPC1
             };
         }
 
+        public override async Task<ResponseForPrayerTimesInCityAndSchool> GetPrayerTimesByCityAndSchool(RequestForPrayerTimesInCityAndSchool request, ServerCallContext context)
+        {
+            var client = httpClientFactory.CreateClient();
+            PrayerTimes prayerTimes = await GetPrayerTimesWithSchool(request, client);
+
+            return new ResponseForPrayerTimesInCityAndSchool
+            {
+                Asr = prayerTimes.Results.Datetime[0].Times.Asr,
+                Dhuhr = prayerTimes.Results.Datetime[0].Times.Dhuhr,
+                Fajr = prayerTimes.Results.Datetime[0].Times.Fajr,
+                Isha = prayerTimes.Results.Datetime[0].Times.Isha,
+                Maghrib = prayerTimes.Results.Datetime[0].Times.Maghrib,
+                TimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                City = request.City,
+                School = request.School
+            };
+        }
+
         public override async Task GetPrayerTimesByCityStream(
             RequestForPrayerTimesInCity request,
             IServerStreamWriter<ResponseForPrayerTimesInCity> responseStream,
@@ -66,9 +84,106 @@ namespace GRPC1
             }
         }
 
+        public override async Task GetPrayerTimesByCityAndSchoolStream(RequestForPrayerTimesInCityAndSchool request, IServerStreamWriter<ResponseForPrayerTimesInCityAndSchool> responseStream, ServerCallContext context)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            for (int i = 0; i < 30; i++)
+            {
+                if (context.CancellationToken.IsCancellationRequested)
+                {
+                    logger.LogInformation("service request cancelled...");
+                    break;
+                }
+
+                var prayerTimes = await GetPrayerTimesWithSchool(request, client);
+                await responseStream.WriteAsync(new ResponseForPrayerTimesInCityAndSchool
+                {
+                    Asr = prayerTimes.Results.Datetime[0].Times.Asr,
+                    Dhuhr = prayerTimes.Results.Datetime[0].Times.Dhuhr,
+                    Fajr = prayerTimes.Results.Datetime[0].Times.Fajr,
+                    Isha = prayerTimes.Results.Datetime[0].Times.Isha,
+                    Maghrib = prayerTimes.Results.Datetime[0].Times.Maghrib,
+                    TimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                    School = request.School,
+                    City = request.City
+                });
+                await Task.Delay(1000);
+            }
+        }
+
+        public override async Task<ResponseForPrayerTimesForMultipleCities> GetMultplePrayerTimesBasedOnCity(IAsyncStreamReader<RequestForPrayerTimesInCity> requestStream, ServerCallContext context)
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = new ResponseForPrayerTimesForMultipleCities
+            {
+                PrayerTimes = { }
+            };
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                if (context.CancellationToken.IsCancellationRequested)
+                {
+                    logger.LogInformation("service request cancelled...");
+                    break;
+                }
+
+                var prayerTimes = await GetPrayerTimes(request, client);
+                response.PrayerTimes.Add(new ResponseForPrayerTimesInCity
+                {
+                    Asr = prayerTimes.Results.Datetime[0].Times.Asr,
+                    Dhuhr = prayerTimes.Results.Datetime[0].Times.Dhuhr,
+                    Fajr = prayerTimes.Results.Datetime[0].Times.Fajr,
+                    Isha = prayerTimes.Results.Datetime[0].Times.Isha,
+                    Maghrib = prayerTimes.Results.Datetime[0].Times.Maghrib,
+                    TimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                    City = request.City
+                });
+            }
+            return response;
+        }
+
+        public override async Task<ResponseForPrayerTimesForMultipleCitiesAndSchools> GetMultplePrayerTimesBasedOnCityAndSchool(IAsyncStreamReader<RequestForPrayerTimesInCityAndSchool> requestStream, ServerCallContext context)
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = new ResponseForPrayerTimesForMultipleCitiesAndSchools
+            {
+                PrayerTimes = { }
+            };
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                if (context.CancellationToken.IsCancellationRequested)
+                {
+                    logger.LogInformation("service request cancelled...");
+                    break;
+                }
+
+                var prayerTimes = await GetPrayerTimesWithSchool(request, client);
+                response.PrayerTimes.Add(new ResponseForPrayerTimesInCityAndSchool
+                {
+                    Asr = prayerTimes.Results.Datetime[0].Times.Asr,
+                    Dhuhr = prayerTimes.Results.Datetime[0].Times.Dhuhr,
+                    Fajr = prayerTimes.Results.Datetime[0].Times.Fajr,
+                    Isha = prayerTimes.Results.Datetime[0].Times.Isha,
+                    Maghrib = prayerTimes.Results.Datetime[0].Times.Maghrib,
+                    TimeStamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                    City = request.City,
+                    School = request.School
+                });
+            }
+            return response;
+        }
+
         private static async Task<PrayerTimes> GetPrayerTimes(RequestForPrayerTimesInCity request, HttpClient client)
         {
-            var response = await client.GetStringAsync($"https://api.pray.zone/v2/times/today.json?city={request.City}&school={request.School}");
+            var response = await client.GetStringAsync($"https://api.pray.zone/v2/times/today.json?city={request.City}");
+
+            var prayerTimes = PrayerTimes.FromJson(response);
+            return prayerTimes;
+        }
+
+        private static async Task<PrayerTimes> GetPrayerTimesWithSchool(RequestForPrayerTimesInCityAndSchool request, HttpClient client)
+        {
+            var response = await client.GetStringAsync($"https://api.pray.zone/v2/times/today.json?city={request.City}&school={(int)request.School}");
 
             var prayerTimes = PrayerTimes.FromJson(response);
             return prayerTimes;
